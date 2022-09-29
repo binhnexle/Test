10ff6a493ffa954096d3482c411fa1e73ec8ee4a
binhhunep <binhdt@nexlesoft.com>
2022-09-29 08:25:31 +0700

    change API

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
-export const BASE_URL = 'https://www.pinkvilla.com';
-export const URL_GALLERY = `${BASE_URL}/photo-gallery-feed-page`;
+export const BASE_URL = 'https://englishapi.pinkvilla.com';
+export const URL_GALLERY = `${BASE_URL}/app-api/v1/photo-gallery-feed-page`;
 export const URL_GALLERY_WITH_PAGE = `${URL_GALLERY}/page`;
 export const DEFAULT_IMAGE = 'https://placehold.jp/30/dd6699/ffffff/300x150.png?text=placeholder+image';
 export const FORMAT_DATE = `MMM DD,YYYY,hh:mm a`;
